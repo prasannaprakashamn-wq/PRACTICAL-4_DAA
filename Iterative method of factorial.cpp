be1db2be06c158be8d01067f052f9a0062c8b6de
Pasheddula Prasanna Kumari <prasannaprakashamn@gmail.com>
2026-09-06 15:33:26 +0530

    Create Iterative method of factorial.cpp

---
+++ Iterative method of factorial.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+long long factorialIterative(int n)
+{
+    long long fact = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Factorial using Iterative Method = "
+         << factorialIterative(n);
+    cout << "\nTime Complexity: O(n)";
+
+    return 0;
+}
